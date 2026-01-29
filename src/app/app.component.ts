37c8ed13f62d2a4465a08d3f0923ace20a7a818e
Chanjirawan <Chanjirawan.y@kkumail.com>
2026-01-29 20:27:39 +0700

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,38 @@ const COLORS = {
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  standalone: false,
 })
 export class AppComponent {
   title = 'bn-pokedex';
+
+  myPokedex: any[] = [];
+  searchResult: any[] = [];
+
+  isModalOpen = false;
+  searchName = '';
+  searchType = '';
+
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  closeModal(event: Event) {
+    if ((event.target as HTMLElement).classList.contains('modal-overlay')) {
+      this.isModalOpen = false;
+    }
+  }
+
+  searchPokemon() {
+    this.searchResult = [];
+  }
+
+  addPokemon(pokemon: any) {
+    this.myPokedex.push(pokemon);
+  }
+
+  removePokemon(id: any) {
+    this.myPokedex = this.myPokedex.filter(p => p.id !== id);
+  }
 }
+
